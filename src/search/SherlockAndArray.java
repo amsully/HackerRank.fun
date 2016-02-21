70fd1feca2f81abb360eed429723d8bcdeabd67f
amsully <sullivan1513@gmail.com>
2016-02-20 19:50:18 -0500

    Updating 'implementation', solved sherlock and array, beginning game of thrones.

---
+++ src/search/SherlockAndArray.java
@@ -0,0 +1,7 @@
+package search;
+
+public class SherlockAndArray {
+
+        
+        
+}
---
+++ src/strings/GameOfThronesI.java
@@ -0,0 +1,43 @@
+package strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class GameOfThronesI {
+        
+        public static void main(String[] args) throws IOException{
+                
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                
+                String val = br.readLine();
+                
+                char[] valArr = val.toCharArray();
+                
+                Arrays.sort(valArr);
+                
+                
+                char prev = valArr[0];
+                
+                int count = -1; // count -1 for base case where prev = curr;
+                boolean valid = true;
+                for(char c : valArr){
+                        if(prev == c){
+                                count++;
+                        }else{
+                                if(count %2 == 1 && valid){
+                                      valid = false;  
+                                }else{
+                                        System.out.println("NO");
+                                        return;
+                                }
+                        }
+                }
+                System.out.println("YES");
+                
+                
+                
+        }
+        
+}
